f63ebb898463d2978cefea526d3074a3b1217fd1
Omer Liv <omerleave@gmail.com>
2021-07-25 13:09:44 +0300

    sunday demo3

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -106,7 +106,7 @@ async function add(user) {
             about: user.about,
             from:user.from,
             imgUrl: user.imgUrl,
-            memberSince: date.getFullYear(),
+            memberSince: new Date().getFullYear(),
             reviews: user.reviews || [],
             skills: user.skills || []
         }
