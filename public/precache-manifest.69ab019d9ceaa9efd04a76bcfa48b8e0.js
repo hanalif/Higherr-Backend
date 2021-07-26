3f5a0b3a9a7ec0581e7ef8a5c3ddecd3727e67a5
Omer Liv <omerleave@gmail.com>
2021-07-26 09:39:59 +0300

    first commit

---
+++ public/precache-manifest.69ab019d9ceaa9efd04a76bcfa48b8e0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27705c6bd56aa64beb5",
+    "url": "/css/app.095eed11.css"
+  },
+  {
+    "revision": "72ff9bdb03a89852b939",
+    "url": "/css/chunk-vendors.37b3d8f8.css"
+  },
+  {
+    "revision": "cfe0d9778b201591dbe4b0c87ab0e5a8",
+    "url": "/fonts/BarlowSemiCondensed-Regular.cfe0d977.ttf"
+  },
+  {
+    "revision": "4fa3683005fa20dd058f47b9cfd411df",
+    "url": "/fonts/FjallaOne.4fa36830.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9b72a311bb35194ff57bd6276ffbe2e7",
+    "url": "/fonts/macan-light-0835cc4494a2fbedfb7b6f618ce90f1e.9b72a311.woff2"
+  },
+  {
+    "revision": "3d263b5d897300221339f211f2b74e5e",
+    "url": "/fonts/macan-regular-92412b97c2a1e21c95ba8d938a2c55df.3d263b5d.woff2"
+  },
+  {
+    "revision": "017065d8e5a2b18240afbf1c6415bb5f",
+    "url": "/img/hero1.017065d8.png"
+  },
+  {
+    "revision": "8e4c1d3aa93c82cc1b5fe86d5959d9e6",
+    "url": "/img/hero2.8e4c1d3a.png"
+  },
+  {
+    "revision": "e5d893323b750c58945672c60686ba65",
+    "url": "/img/hero3.e5d89332.png"
+  },
+  {
+    "revision": "8dbfa30842fbe7ec08aec1108a06e531",
+    "url": "/img/star.8dbfa308.jpg"
+  },
+  {
+    "revision": "9b9d144bdad75d06ac0bc9d0723b68f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27705c6bd56aa64beb5",
+    "url": "/js/app.fae09d2e.js"
+  },
+  {
+    "revision": "72ff9bdb03a89852b939",
+    "url": "/js/chunk-vendors.90121e32.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
